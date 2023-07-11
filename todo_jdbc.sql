870ca0c0f4883602c0e97589d728cccce3b21b5c
Daris02 <hei.raymond.2@gmail.com>
2023-07-11 12:06:02 +0300

    fix: update contraints

--- todo_jdbc.sql
+++ todo_jdbc.sql
@@ -4,9 +4,9 @@
 
 CREATE TABLE "todo" (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(100),
+    title VARCHAR(100) NOT NULL,
     description TEXT,
     deadline TIMESTAMP CHECK (deadline > current_date),
-    priority INT CHECK (priority <= 10 AND priority >= 0),
-    done BOOLEAN
+    priority INT CHECK (priority <= 10 AND priority >= 0) DEFAULT 5,
+    done BOOLEAN DEFAULT false
 );

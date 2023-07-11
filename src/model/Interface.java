880355d6e7af942177b41969c709d3e796108ba5
mampiononavoahary <hei.zoarisoa@gmail.com>
2023-07-11 20:45:39 +0300

    fix:this is the interfance but it's not OK

---
+++ src/model/Interface.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Interface {
+    public static void main(String[] args) {
+        System.out.println("=====TODO Menu=====");
+        System.out.println("1 : Show all todo");
+        System.out.println("2 : ??");
+        System.out.println("3 : Change todo");
+        System.out.println("4 : Insert todo");
+        System.out.println("5 : Delete Todo");
+        System.out.println("6 : Quit");
+    }
+}

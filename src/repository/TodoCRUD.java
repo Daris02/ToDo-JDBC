9d63d55001a8721ec0aa8b274abcc4e99aae6411
Daris02 <hei.raymond.2@gmail.com>
2023-07-12 10:49:45 +0300

    fix: remove import not used

--- src/repository/TodoCRUD.java
+++ src/repository/TodoCRUD.java
@@ -1,14 +1,10 @@
 package repository;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import databaseconnection.ConnectionDB;
